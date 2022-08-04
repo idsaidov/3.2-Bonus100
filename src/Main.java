3c9285721fd3d669b2c536185971fa642097a97a
idsaidov <id.saidov@yandex.ru>
2022-08-04 22:27:13 +0300

    Correction3

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,21 @@
 public class Main {
     public static void main(String[] args) {
-        int score = 30;
+        int score = 100;
         int replenishment = 300;
-        int bonus = 0;
-
+        int bonus;
         if (replenishment > 1000) {
             bonus = replenishment / 100;
             System.out.println("Клиент пополнил счёт на " + replenishment + " рублей. Накопил " + bonus + " бонусов.");
         }else {
+            bonus = 0;
             System.out.println("Клиент пополнил счёт на " + replenishment + " рублей. Накопил " + bonus + " бонусов.");
             System.out.println("Если клиент пополняет счёт более чем на 1000 рублей, то сотовый оператор дарит ему по 1 рублю за каждые полные 100 рублей пополнения.");
         }
         int inTotal = score + replenishment + bonus;
 
         System.out.println("Итоговая сумма на счету клиента: " + inTotal + "рублей.");
+
+
 
 
 
