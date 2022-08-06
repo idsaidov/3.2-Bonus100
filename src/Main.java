212b457aef02a8ac6c47348084ef498f2bf488b6
idsaidov <id.saidov@yandex.ru>
2022-08-06 17:41:37 +0300

    V2

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,14 @@
 public class Main {
     public static void main(String[] args) {
         int score = 100;
-        int replenishment = 300;
-        int bonus;
+        int replenishment = 1300;
+//        int bonus;
+        int bonus = 0;
         if (replenishment > 1000) {
             bonus = replenishment / 100;
             System.out.println("Клиент пополнил счёт на " + replenishment + " рублей. Накопил " + bonus + " бонусов.");
         }else {
-            bonus = 0;
+//            bonus = 0;
             System.out.println("Клиент пополнил счёт на " + replenishment + " рублей. Накопил " + bonus + " бонусов.");
             System.out.println("Если клиент пополняет счёт более чем на 1000 рублей, то сотовый оператор дарит ему по 1 рублю за каждые полные 100 рублей пополнения.");
         }
